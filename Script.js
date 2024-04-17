44f7c04b830aa09b4b52d53cd9108635c940964b
kathir-8 <kathirganesh6@gmail.com>
2024-04-17 12:50:55 +0530

    Finished

---
+++ Script.js
@@ -0,0 +1,34 @@
+
+setTimeout(()=>{
+    document.getElementById("Cont").innerHTML="<b> 10 </b>"
+    setTimeout(()=>{
+     document.getElementById("Cont").innerHTML="<b> 9 </b>"
+        setTimeout(()=>{
+        document.getElementById("Cont").innerHTML="<b> 8 </b>"
+        setTimeout(()=>{
+            document.getElementById("Cont").innerHTML="<b> 7 </b>"
+            setTimeout(()=>{
+                document.getElementById("Cont").innerHTML="<b> 6 </b>"
+                setTimeout(()=>{
+                    document.getElementById("Cont").innerHTML="<b> 5 </b>"
+                    setTimeout(()=>{
+                     document.getElementById("Cont").innerHTML="<b> 4 </b>"
+                        setTimeout(()=>{
+                        document.getElementById("Cont").innerHTML="<b> 3 </b>"
+                        setTimeout(()=>{
+                            document.getElementById("Cont").innerHTML="<b> 2 </b>"
+                            setTimeout(()=>{
+                                document.getElementById("Cont").innerHTML="<b> 1 </b>"
+                                setTimeout(()=>{
+                                    document.getElementById("Cont").innerHTML="<b> Happy Indepedence Day </b>"
+                                 },2000)
+                             },1000)
+                          },1000)
+                        },1000)
+                    },1000)
+                },1000)
+             },1000)
+          },1000)
+        },1000)
+    },1000)
+},1000)
